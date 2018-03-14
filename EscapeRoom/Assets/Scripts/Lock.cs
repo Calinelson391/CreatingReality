e809a9bd67f5999416132c9a8ffbfe1606f4b869
Calista Nelson <calistan@usc.edu>
2018-03-13 22:51:25 -0700

    Lock and Key

---
+++ EscapeRoom/Assets/Scripts/Lock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        print("collision");
+        if (collision.gameObject.GetComponent<Key>() != null)
+        {
+            print("collission with key");
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
